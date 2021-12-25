1935b1583a6ed7b7302567cf8b50e02627c57af5
BeAnjara <hbeanjarahoussen@gmail.com>
2021-12-25 05:58:53 +0300

    :fire: Home Screen > Add route logic to control home screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+/* eslint-disable */
 
 function App() {
   return (
